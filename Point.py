0e13509c35f28dd4b9e397679d468543172d872b
Pranav <thepranavtotla@gmail.com>
2019-08-29 12:07:16 +0530

    Introduced initialization using **kwargs

--- Point.py
+++ Point.py
@@ -1,10 +1,14 @@
 class Point:
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if len(args) == 1:
             self.point = args[0]
             self.x = self.point[0]
             self.y = self.point[1]
-        if len(args) == 2:
-            self.point = [args[0], args[1]]
+        elif len(args) == 2:
             self.x = args[0]
             self.y = args[1]
+            self.point = [self.x, self.y]
+        elif len(args) == 0:
+            self.x = kwargs.get('x')
+            self.y = kwargs.get('y')
+            self.point = [self.x, self.y]
