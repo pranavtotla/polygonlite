8a5891931623dbc7f8153c016bd4cf9ab78dc2e1
Pranav <thepranavtotla@gmail.com>
2019-08-29 16:00:40 +0530

    Add length method to Edge

--- Polygon/Edge.py
+++ Polygon/Edge.py
@@ -1,5 +1,10 @@
+import math
+from Polygon.Point import Point
 
 class Edge:
     def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
+        self.point1 = Point(point1)
+        self.point2 = Point(point2)
+
+    def length(self):
+        return math.sqrt(((self.point2.x - self.point1.x) ** 2) + ((self.point2.y - self.point1.y) ** 2))
