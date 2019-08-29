f813e6702a1e3cfb7ec858b2f89f4379358bfb6e
Pranav <thepranavtotla@gmail.com>
2019-08-29 18:48:45 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='polygon-lite',
+    version='0.0.1',
+    packages=['Polygon'],
+    url='https://github.com/pranavtotla/polygon-lite',
+    license='MIT',
+    author='Pranav Totla',
+    author_email='',
+    description='A lightweight module for polygon, edge and point operations.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+    ],
+)
