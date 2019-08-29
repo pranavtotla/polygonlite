bec7afc67905a4f185d1db8ca1239db5426aa6d0
Pranav <thepranavtotla@gmail.com>
2019-08-29 16:26:47 +0530

    Added is_between to Services.py

--- Polygon/Services.py
+++ Polygon/Services.py
@@ -1,1 +1,11 @@
+def is_between(number1, number2, number3):
+    if number2 >= number1:
+        if number2 <= number3:
+            return True
+    elif number2 >= number3:
+        if number1 <= number1:
+            return True
+    return False
+
+
 dot = lambda X, Y: sum(map(lambda x, y: x * y, X, Y))
