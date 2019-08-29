48e861c0539922267e51696db6127033857b3515
Pranav <thepranavtotla@gmail.com>
2019-08-29 16:18:40 +0530

    Added midpoint method to Edge class

--- Polygon/Edge.py
+++ Polygon/Edge.py
@@ -1,6 +1,6 @@
 import math
 from Polygon.Point import Point, THRESHOLD
-
+from Polygon.Services import dot
 
 class Edge:
     def __init__(self, point1, point2):
@@ -20,3 +20,6 @@ class Edge:
         if self.point2.x - self.point1.x < THRESHOLD:
             return 'inf'
         return (self.point2.y - self.point1.y) / (self.point2.x - self.point1.x)
+
+    def midpoint(self):
+        return (self.point1 + self.point2) / 2
