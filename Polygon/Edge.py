6355a6bad8f4296e4f67a06bdf95b9933e796f76
Pranav <thepranavtotla@gmail.com>
2019-08-29 16:09:28 +0530

    Modified Edge.length logic

--- Polygon/Edge.py
+++ Polygon/Edge.py
@@ -1,13 +1,15 @@
 import math
 from Polygon.Point import Point
 
+
 class Edge:
     def __init__(self, point1, point2):
         self.point1 = Point(point1)
         self.point2 = Point(point2)
 
     def length(self):
-        return math.sqrt(((self.point2.x - self.point1.x) ** 2) + ((self.point2.y - self.point1.y) ** 2))
+        return math.sqrt(self.vector().square().sum())
 
     def vector(self):
-        return self.point2 - self.point1
+        return Point(self.point2 - self.point1)
+
