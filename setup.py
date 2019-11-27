fa0dddd90d6b62510d56383198ef8040a7360091
Pranav Totla <thepranavtotla@gmail.com>
2019-11-27 14:29:04 +0530

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='polygonlite',
-    version='0.0.8.5',
+    version='0.1.0',
     packages=['polygonlite'],
     url='https://github.com/pranavtotla/polygonlite',
     license='MIT',
@@ -15,5 +15,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2',
     ],
-    download_url='https://github.com/pranavtotla/polygonlite/archive/v0.0.8.5.tar.gz'
+    download_url='https://github.com/pranavtotla/polygonlite/archive/v0.1.0.tar.gz'
 )
